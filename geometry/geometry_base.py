72cf77516d4a35736d73e0930742447420b1e2ab
Fubi00 <160527008+Fubi00@users.noreply.github.com>
2026-06-21 05:30:17 +0200

    Lagt til de siste geometri linjene for en kvart skrog

--- geometry/geometry_base.py
+++ geometry/geometry_base.py
@@ -47,31 +47,89 @@ def generate_waterline(L, B_max, x1_factor, y1_factor, x2_factor, y2_factor, num
         
     return waterline_points
 
-def generate_station_profile(y_wl, T, y1_factor, z1_factor, y2_factor, z2_factor, num_points=15):
+def generate_keel_profile(L, T, z1_factor=0.0, z2_factor=0.2, num_points=20):
     """
-    Genererer (Y, Z) punkter for en halvstasjon (ett spant på en side).
-    y_wl: Bredden skroget har på dette spesifikke stedet (fra vannlinjekurven).
-    T: Totalt dypgående (draft).
-    Factors: Tall mellom 0.0 og 1.0 som styrer fyldigheten.
+    Genererer (X, Z) koordinater for kjøllinjen fra midtskip til baug.
+    z1_factor og z2_factor styrer hvor raskt kjølen svinger opp mot baugen.
     """
-    # P0: Kjølen (X=0 i lokal spant-akse, Z=-T)
+    half_L = L / 2
+    
+    # P0: Midtskipet (Båten er på sitt dypeste, Z = -T)
     p0 = (0.0, -T)
+    # P1 & P2: Kontrollpunkter som optimalisatoren kan justere for å endre bunnprofilen
+    p1 = (0.25 * half_L, -T + (z1_factor * T))
+    p2 = (0.75 * half_L, -T + (z2_factor * T))
+    # P3: Baugtuppen (Her treffer kjølen vannlinjen, Z = 0.0)
+    p3 = (half_L, 0.0)
     
-    # P1 & P2: Kontrollpunkter, strengt begrenset innforbi y_wl
-    p1 = (y1_factor * y_wl, -T + (z1_factor * T))
-    p2 = (y2_factor * y_wl, -T + (z2_factor * T))
+    keel_points = []
+    for i in range(num_points):
+        t = i / (num_points - 1)
+        
+        x = (1-t)**3 * p0[0] + 3*(1-t)**2 * t * p1[0] + 3*(1-t) * t**2 * p2[0] + t**3 * p3[0]
+        z = (1-t)**3 * p0[1] + 3*(1-t)**2 * t * p1[1] + 3*(1-t) * t**2 * p2[1] + t**3 * p3[1]
+        
+        keel_points.append((x, z))
+        
+    return keel_points
     
-    # P3: Vannlinjen
+def generate_updated_station(y_wl, z_kjol, y1_factor=0.5, z1_factor=0.0, y2_factor=1.0, z2_factor=0.5, num_points=15):
+    """
+    Genererer (Y, Z) for et spant, tilpasset lokal kjølhøyde (z_kjol) og bredde (y_wl).
+    z1_factor og z2_factor styrer høydeposisjonen relativt mellom kjøl og vannlinje.
+    """
+    # P0: Start på den lokale kjølen (Senterlinje Y=0, Z=z_kjol)
+    p0 = (0.0, z_kjol)
+    
+    # P3: Ende på vannlinjen (Y=y_wl, Z=0.0)
     p3 = (y_wl, 0.0)
+    
+    # Høyden på dette spesifikke spantet (avstanden fra kjøl til vannlinje)
+    h_local = abs(z_kjol)
+    
+    # P1 & P2: Kontrollpunkter beskyttet mot å gå utenfor y_wl, 
+    # og skalert etter det lokale spantets høyde
+    p1 = (y1_factor * y_wl, z_kjol + (z1_factor * h_local))
+    p2 = (y2_factor * y_wl, z_kjol + (z2_factor * h_local))
     
     station_points = []
     for i in range(num_points):
         t = i / (num_points - 1)
         
-        # Kubisk Bézier-formel for Y og Z
         y = (1-t)**3 * p0[0] + 3*(1-t)**2 * t * p1[0] + 3*(1-t) * t**2 * p2[0] + t**3 * p3[0]
         z = (1-t)**3 * p0[1] + 3*(1-t)**2 * t * p1[1] + 3*(1-t) * t**2 * p2[1] + t**3 * p3[1]
         
         station_points.append((y, z))
         
     return station_points
+
+    def generate_3d_hull_mesh(L, B_max, T, num_stations=20, num_points_per_station=15):
+    """
+    Hovedfunksjon for å generere et strukturert 3D-punktnettverk for forskipet.
+    """
+    hull_mesh = [] # En liste som skal holde på alle stasjonene
+    
+    # 1. Definer master-kurvene (Vannlinje og Kjølprofil)
+    # I en ferdig optimalisator vil faktorene under være variabler fra 'Godzilla'
+    
+    for i in range(num_stations):
+        # Fordel stasjonene jevnt fra midtskipet (X=0) til baugen (X = L/2)
+        x_local = (i / (num_stations - 1)) * (L / 2)
+        
+        # 2. FINN LOKAL BREDDE (Y_wl) FRA VANNLINJEKURVEN VED DENNE X
+        # (For enkelhets skyld bruker vi en forenklet parabel her i eksempelet, 
+        # men i koden din vil du løse Bézier-ligningen for x_local)
+        y_wl = (B_max / 2) * (1 - (x_local / (L / 2))**2) 
+        
+        # 3. FINN LOKAL KJØLDYBDE (Z_kjol) FRA PROFILKURVEN VED DENNE X
+        # Kjølen svinger oppover fra -T på midtskipet til 0 i baugen
+        z_kjol = -T * (1 - (x_local / (L / 2))**2)
+        
+        # 4. GENERER SPANTET BASERT PÅ DE LOKALE VERDIENE
+        # Her kaller vi den oppdaterte stasjonsfunksjonen
+        station_points = generate_updated_station(y_wl, z_kjol, num_points_per_station)
+        
+        # Lagre stasjonen sammen med sin X-koordinat
+        hull_mesh.append({'X': x_local, 'Points': station_points})
+        
+    return hull_mesh
